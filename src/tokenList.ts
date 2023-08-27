9a89dc5365fda8792e9182b510fbe20cb3511055
Mike Conrad <mike@enxo.co>
2023-08-27 11:16:13 -0400

    Revert "Refactored to show correct time.  Still needs to be made dynamic"
    This reverts commit 21b6f0609eb9d27f4ed62b082b647671bc56c020.

--- src/tokenList.ts
+++ src/tokenList.ts
@@ -4,12 +4,7 @@ import { tokens } from "./tokens";
 export function tokenList() {
   const element = document.createElement("div");
   element.classList.add("test");
-  const div = document.createElement('div')
-  div.append('<form id="tokens">')
-  document.querySelector<HTMLDivElement>("#app")!.innerHTML += '<form id="tokens">'
-  tokens.map(async (token) => {
-    const el = await displayTokenListItem(token.account, token.secret, element)
-    document.querySelector<HTMLDivElement>("#app")!.innerHTML += el
-  })
-
+  return `<form id="tokens">${tokens.map((token) =>
+    displayTokenListItem(token.account, token.secret, element)
+  )}</form>`;
 }
--- src/tokenListItem.ts
+++ src/tokenListItem.ts
@@ -1,28 +1,6 @@
 import {displayToken} from './token'
-import Timer from './tokenTimer';
-import totp from 'totp-generator'
-const period = 30
-const digits = 6
-let token = ''
-async function generateTOTP(secret: string): Promise<{secondsSinceStart: number, secondsRemaining: number}>{
-  const token = totp(secret.replace(/ /, '').trim(), {
-    digits,
-    period,
-  })
+export function displayTokenListItem(account: string, secret: string, element: HTMLDivElement){
 
-  const secondsSinceEpoch = Math.ceil(Date.now() / 1000) - 1
-  const secondsSinceStart = 0 + (secondsSinceEpoch % period)
-  const secondsRemaining = period - (secondsSinceEpoch % period)
-  return {
-    secondsSinceStart,
-    secondsRemaining
-  }
-}
-export async function displayTokenListItem(account: string, secret: string, element: HTMLDivElement){
-  const period = 30
-  const digits = 6
-  const {secondsRemaining, secondsSinceStart} = await generateTOTP(secret)
-  const token = await displayToken(secret)
     return element.innerHTML = `<div class="fieldset-wrapper">
     <fieldset>
       <label>Account</label>
@@ -30,10 +8,7 @@ export async function displayTokenListItem(account: string, secret: string, elem
     </fieldset>
     <fieldset>
       <label>Code</label>
-      <p data-test-id="secret">${await displayToken(secret)}</p>
+      <p data-test-id="secret">${displayToken(secret)}</p>
     </fieldset>
-    <fieldset>
-    <label></label>
-    <p>${await Timer(secondsSinceStart, secondsRemaining)}</p>
     </div>`;
 }
